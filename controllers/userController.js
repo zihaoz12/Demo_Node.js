33e37d3f8e584becdb7bc6dff6b1290dc163d563
zihaoz12 <zzhaa12@gmail.com>
2019-05-30 00:42:37 -0700

    update register msg

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,18 +9,34 @@ router.get('/register',(req,res)=>{
 });
 
 router.post('/register',(req,res)=>{
-    let newUser = new User({username:req.body.username});
-    User.register(newUser,req.body.password,(err,user)=>{
-        if(err){
-            req.flash("error",err.message)
-            return res.render('users/register.ejs')
-        }
-        passport.authenticate("local")(req,res,function(){
-            console.log("create user--->",user.username)
-            req.flash('success',"you have signed up successfully")
-            res.redirect('/posts')
-        });
-    });
+    const {username, password, confirm_password} = req.body;
+    if(!username || !password || !confirm_password){
+        req.flash("error","Please enter all fields");
+        res.redirect('back')
+    }else if(password != confirm_password){
+        req.flash("error","Passwords do not match");
+        res.redirect('back')
+    }else{
+        User.findOne({username:username}).then(user=>{
+            if(user){
+                req.flash("error","Username is already exists");
+                res.redirect('back')
+            }else{
+                let newUser = new User({username:req.body.username});
+                User.register(newUser,req.body.password,(err,user)=>{
+                    if(err){
+                        req.flash("error",err.message)
+                        return res.render('users/register.ejs')
+                    }
+                passport.authenticate("local")(req,res,function(){
+                        console.log("create user--->",user.username)
+                        req.flash('success',"you have signed up successfully")
+                        res.redirect('/posts')
+                    });
+                });
+            }
+        })   
+    }
 });
 
 
